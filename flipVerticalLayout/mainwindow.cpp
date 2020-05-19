98cf19c8ae6a15bec95e9f91b622367200c9a015
Laszlo Vincze <lvincze@avinger.local>
2020-05-19 03:12:58 -0700

    resize

--- flipVerticalLayout/mainwindow.cpp
+++ flipVerticalLayout/mainwindow.cpp
@@ -15,10 +15,12 @@ MainWindow::MainWindow(QWidget *parent)
     ui->frameBL->hide();
     const int w{m_sceneSize};
     const int h{m_sceneSize * m_widthHeight.second / m_widthHeight.first};
-    this->setMinimumSize(w,h);
-    this->setMaximumSize(w,h);
-    ui->graphicsView->setMaximumSize(h,h);
-    ui->graphicsView->setMinimumSize(h,h);
+//    this->setMinimumSize(w,h);
+//    this->setMaximumSize(w,h);
+    resize(w,h);
+//    ui->graphicsView->setMaximumSize(h,h);
+//    ui->graphicsView->setMinimumSize(h,h);
+    ui->graphicsView->resize(w,w);
 }
 
 MainWindow::~MainWindow()
